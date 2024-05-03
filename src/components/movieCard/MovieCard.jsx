fea71bd7a2cc8c4f2e2f7228a55675452677f5f2
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-05-04 01:05:44 +0600

    create movie card and passing props for data maping

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,148 +1,30 @@
 import React from 'react';
+import Rating from '../rating/Rating';
+import { getImgUrl } from '../../utils/Cine-Utility';
 
-
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
     return (
-        <>
-             {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-             <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-          {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-              <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-          {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-              <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-          {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-              <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-          {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-              <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-          {/* Begin Card */}
-          <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={MovieImage}
-              alt=""
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">Iron Man</h3>
-              <p className="text-[#575A6E] text-sm mb-2">
-                Action/Adventure/Sci-fi
-              </p>
-              <Rating />
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src={Tag} alt="" />
-                <span>$100 | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-          {/* End Card */}
-        </>
+        <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+                    <img
+                      className="w-full object-cover"
+                      src={getImgUrl(movie.cover)}
+                      alt=""
+                    />
+                    <figcaption className="pt-4">
+                      <h3 className="text-xl mb-1">{movie.title}</h3>
+                      <p className="text-[#575A6E] text-sm mb-2">
+                        {movie.genre}
+                      </p>
+                     <Rating />
+                      <a
+                        className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+                        href="#"
+                      >
+                        <img src="./assets/tag.svg" alt="" />
+                        <span>${movie.price} | Add to Cart</span>
+                      </a>
+                    </figcaption>
+                  </figure> 
     );
 };
 
--- src/components/sidebar/movieList/MovieList.jsx
+++ src/components/sidebar/movieList/MovieList.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Sidebar from "../Sidebar";
-import MovieImage from '../../../assets/images/movie-1.png'
-import Tag from '../../../assets/icons/tag.svg'
-import Rating from '../../rating/Rating';
 import { getAllMovies } from "../../../data/Movies";
-import { getImgUrl } from "../../../utils/Cine-Utility";
+import MovieCard from "../../movieCard/MovieCard";
 
 const MovieList = () => {
   const movies = getAllMovies();
@@ -24,27 +21,7 @@ const MovieList = () => {
             {/* Begin Card */}
             {
                 movies.map(movie=>(
-                    <figure key={movie.id} className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-                    <img
-                      className="w-full object-cover"
-                      src={getImgUrl(movie.cover)}
-                      alt=""
-                    />
-                    <figcaption className="pt-4">
-                      <h3 className="text-xl mb-1">{movie.title}</h3>
-                      <p className="text-[#575A6E] text-sm mb-2">
-                        {movie.genre}
-                      </p>
-                     <Rating />
-                      <a
-                        className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                        href="#"
-                      >
-                        <img src="./assets/tag.svg" alt="" />
-                        <span>${movie.price} | Add to Cart</span>
-                      </a>
-                    </figcaption>
-                  </figure> 
+                   <MovieCard key={movie.id} movie={movie}/>
                 ))
             }
           
